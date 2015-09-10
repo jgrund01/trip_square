94a5de6f8ba67d96e8f01a3942bf8d3443c04938
Jordan Grund <jordan.grund@adherial.com>
2015-09-10 14:24:23 -0700

    Updated project name

--- trip_square/urls.py
+++ trip_square/urls.py
@@ -1,4 +1,4 @@
-"""trip_square_django URL Configuration
+"""trip_square URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
