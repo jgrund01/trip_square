c656c28ae59889f5799e3a1be363746459cc450f
Jordan Grund <jordan.grund@adherial.com>
2015-09-10 16:34:37 -0700

    Added initial model schema

--- square/models.py
+++ square/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+from django.utils import timezone
+
+
+class Event(models.Model):
+    event_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    event = models.ForeignKey(Event)
+
+    created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Expense(models.Model):
+
+    event = models.ForeignKey(Event)
+    amount = models.FloatField(default=0)
+    title = models.CharField(max_length=255)
+
+    created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+class Contribution(models.Model):
+
+    created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    amount = models.IntegerField(default=0)
+    event = models.ForeignKey(Event)
+    member = models.ForeignKey(Member)
+
+
